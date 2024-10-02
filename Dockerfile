b13c29de4558c1cc1fe631e99c65b0905c8167fd
Robin Tuszik <mail@robin.gg>
2024-10-03 00:45:44 +0200

    refactor(Dockerfile): optimize package installation and cleanup to reduce image size refactor(Dockerfile): replace wget with ADD for photon jar to simplify Dockerfile refactor(start-photon.sh): simplify index download logic and remove unused code fix(start-photon.sh): update index URL to use German dataset for consistency fix(start-photon.sh): ensure Photon only starts if the search index is present

--- Dockerfile
+++ Dockerfile
@@ -2,17 +2,22 @@ FROM adoptopenjdk/openjdk11:jdk-11.0.11_9
 
 WORKDIR /app
 
-RUN apt-get update && apt-get install -y wget pbzip2 curl
+RUN apt-get update \
+  && apt-get -y install \
+  pbzip2 \
+  wget \
+  && rm -rf /var/lib/apt/lists/*
 
 ARG PHOTON_VERSION
 
-RUN wget https://github.com/komoot/photon/releases/download/${PHOTON_VERSION}/photon-${PHOTON_VERSION}.jar -O /app/photon.jar
+ADD https://github.com/komoot/photon/releases/download/${PHOTON_VERSION}/photon-${PHOTON_VERSION}.jar /app/photon.jar
 
 COPY start-photon.sh /app/start-photon.sh
-RUN chmod +x /app/start-photon.sh
 
-RUN mkdir -p /app/photon_data
+VOLUME /photon/photon_data
+
 
 EXPOSE 2322
 
 CMD ["/app/start-photon.sh"]
+
--- start-photon.sh
+++ start-photon.sh
@@ -1,37 +1,21 @@
 #!/bin/bash
 
-set -eo pipefail
-
+# Set the data directory and index URL
 DATA_DIR="/photon_data"
-TEMP_DIR="/tmp/photon_update"
-INDEX_URL="http://download1.graphhopper.com/public/extracts/by-country-code/nl/photon-db-nl-latest.tar.bz2"
-# INDEX_URL="https://download1.graphhopper.com/public/photon-db-latest.tar.bz2"
-# wget --user-agent="$USER_AGENT" -O - http://download1.graphhopper.com/public/extracts/by-country-code/nl/photon-db-nl-latest.tar.bz2 | bzip2 -cd | tar x
-VERSION_FILE="${DATA_DIR}/version.txt"
+# INDEX_URL="http://download1.graphhopper.com/public/photon-db-latest.tar.bz2"
+INDEX_URL="https://download1.graphhopper.com/public/extracts/by-country-code/de/photon-db-de-latest.tar.bz2"
 
-# Function to download and extract the index
-download_and_extract_index() {
-	echo "Downloading and extracting Photon index..."
-	mkdir -p "$TEMP_DIR"
-	wget -O - "$INDEX_URL" | pbzip2 -cd | tar x -C "$TEMP_DIR"
-
-	# Remove old data and move new data into place
-	rm -rf "${DATA_DIR:?}/*"
-	mv "$TEMP_DIR"/* "$DATA_DIR/"
-	rm -rf "$TEMP_DIR"
-
-	date +%Y%m%d >"$VERSION_FILE"
-	echo "Index downloaded, extracted, and moved successfully."
-}
-
-# Check if we need to download the index
-if [ ! -d "${DATA_DIR}/elasticsearch" ] || [ -z "$(ls -A ${DATA_DIR}/elasticsearch)" ] || [ "$UPDATE_INDEX" = "true" ]; then
-	echo "Downloading index due to missing data or UPDATE_INDEX flag..."
-	download_and_extract_index
-else
-	echo "Using existing data in ${DATA_DIR}/elasticsearch"
+# Download elasticsearch index if it doesn't exist
+if [ ! -d "${DATA_DIR}/elasticsearch" ]; then
+	echo "Downloading search index"
+	wget --progress=dot:giga -O - "$INDEX_URL" | pbzip2 -cd | tar x -C "$DATA_DIR"
 fi
 
-# Start Photon
-echo "Starting Photon..."
-exec java -jar /photon.jar
+# Start photon if elastic index exists
+if [ -d "${DATA_DIR}/elasticsearch" ]; then
+	echo "Starting Photon"
+	exec java -jar /photon.jar
+else
+	echo "Could not start Photon, the search index could not be found"
+	exit 1
+fi
