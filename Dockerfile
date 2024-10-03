6e299cc064b8112876e2856e83c3a40eed9f1a67
Robin Tuszik <mail@robin.gg>
2024-10-03 12:16:16 +0200

    feat(workflow): add optional revision input to build-and-push workflow
    This change allows specifying a revision number for Docker image tags,
    enabling more granular version control.
    
    refactor(Dockerfile): update base image and simplify structure
    
    Switch to using eclipse-temurin as the base image for better support.
    Simplify the Dockerfile by adjusting the working directory and entrypoint.
    
    fix(start-photon.sh): improve error handling and support country-specific DB
    
    Enhance error handling in the start script to provide clearer messages.
    Add support for downloading country-specific Photon DB using COUNTRY_CODE.

--- Dockerfile
+++ Dockerfile
@@ -1,23 +1,19 @@
-FROM adoptopenjdk/openjdk11:jdk-11.0.11_9
+FROM eclipse-temurin:21.0.4_7-jre-noble
 
-WORKDIR /app
-
-RUN apt-get update \
+RUN apt-get update \  
   && apt-get -y install \
   pbzip2 \
   wget \
   && rm -rf /var/lib/apt/lists/*
 
+WORKDIR /photon
 ARG PHOTON_VERSION
 
 ADD https://github.com/komoot/photon/releases/download/${PHOTON_VERSION}/photon-${PHOTON_VERSION}.jar /app/photon.jar
 
-COPY start-photon.sh /app/start-photon.sh
+COPY start-photon.sh ./start-photon.sh
 
 VOLUME /photon/photon_data
-
-
 EXPOSE 2322
 
-CMD ["/app/start-photon.sh"]
-
+ENTRYPOINT /photon/start-photon.sh
--- start-photon.sh
+++ start-photon.sh
@@ -1,21 +1,32 @@
 #!/bin/bash
 
-# Set the data directory and index URL
-DATA_DIR="/photon_data"
-# INDEX_URL="http://download1.graphhopper.com/public/photon-db-latest.tar.bz2"
-INDEX_URL="https://download1.graphhopper.com/public/extracts/by-country-code/de/photon-db-de-latest.tar.bz2"
+# Function to handle errors and exit
+handle_error() {
+	echo "Error: $1"
+	exit 1
+}
 
-# Download elasticsearch index if it doesn't exist
-if [ ! -d "${DATA_DIR}/elasticsearch" ]; then
+# Download elasticsearch index
+if [ ! -d "/photon/photon_data/elasticsearch" ]; then
 	echo "Downloading search index"
-	wget --progress=dot:giga -O - "$INDEX_URL" | pbzip2 -cd | tar x -C "$DATA_DIR"
+	if [[ ! -z "${COUNTRY_CODE}" ]]; then
+		wget --progress=dot:giga -O - http://download1.graphhopper.com/public/extracts/by-country-code/${COUNTRY_CODE}/photon-db-${COUNTRY_CODE}-latest.tar.bz2 | bzip2 -cd | tar x || handle_error "Failed to download or extract country-specific Photon DB"
+	else
+		wget --progress=dot:giga -O - http://download1.graphhopper.com/public/photon-db-latest.tar.bz2 | bzip2 -cd | tar x || handle_error "Failed to download or extract the latest Photon DB"
+	fi
+
+	# Check if the download and extraction were successful
+	if [ ! -d "/photon/photon_data/elasticsearch" ]; then
+		handle_error "Search index directory does not exist after extraction"
+	fi
+else
+	echo "Search index directory already exists"
 fi
 
 # Start photon if elastic index exists
-if [ -d "${DATA_DIR}/elasticsearch" ]; then
+if [ -d "/photon/photon_data/elasticsearch" ]; then
 	echo "Starting Photon"
-	exec java -jar /photon.jar
+	java -jar photon.jar "$@" || handle_error "Failed to start Photon"
 else
-	echo "Could not start Photon, the search index could not be found"
-	exit 1
+	handle_error "Could not start Photon, the search index could not be found"
 fi
