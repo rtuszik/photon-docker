42ec20d0c4b317eb65bdd9fe4be14a923d5b790e
Robin Tuszik <mail@robin.gg>
2024-10-03 16:00:13 +0200

    fix(Dockerfile): correct photon.jar path to match WORKDIR fix(docker-compose.build.yaml): update volume path to match Dockerfile feat(docker-compose.build.yaml): add COUNTRY_CODE environment variable fix(start-photon.sh): remove error handling function and simplify logic fix(start-photon.sh): replace bzip2 with pbzip2 for parallel decompression

--- Dockerfile
+++ Dockerfile
@@ -9,11 +9,14 @@ RUN apt-get update \
 WORKDIR /photon
 ARG PHOTON_VERSION
 
-ADD https://github.com/komoot/photon/releases/download/${PHOTON_VERSION}/photon-${PHOTON_VERSION}.jar /app/photon.jar
+ADD https://github.com/komoot/photon/releases/download/${PHOTON_VERSION}/photon-${PHOTON_VERSION}.jar /photon/photon.jar
 
 COPY start-photon.sh ./start-photon.sh
+RUN chmod +x start-photon.sh
+
 
 VOLUME /photon/photon_data
 EXPOSE 2322
 
 ENTRYPOINT /photon/start-photon.sh
+
--- start-photon.sh
+++ start-photon.sh
@@ -1,32 +1,19 @@
 #!/bin/bash
 
-# Function to handle errors and exit
-handle_error() {
-	echo "Error: $1"
-	exit 1
-}
-
 # Download elasticsearch index
 if [ ! -d "/photon/photon_data/elasticsearch" ]; then
 	echo "Downloading search index"
-	if [[ ! -z "${COUNTRY_CODE}" ]]; then
-		wget --progress=dot:giga -O - http://download1.graphhopper.com/public/extracts/by-country-code/${COUNTRY_CODE}/photon-db-${COUNTRY_CODE}-latest.tar.bz2 | bzip2 -cd | tar x || handle_error "Failed to download or extract country-specific Photon DB"
+	if [[ -n "${COUNTRY_CODE}" ]]; then
+		wget --progress=dot:giga -O - http://download1.graphhopper.com/public/extracts/by-country-code/${COUNTRY_CODE}/photon-db-${COUNTRY_CODE}-latest.tar.bz2 | pbzip2 -cd | tar x
 	else
-		wget --progress=dot:giga -O - http://download1.graphhopper.com/public/photon-db-latest.tar.bz2 | bzip2 -cd | tar x || handle_error "Failed to download or extract the latest Photon DB"
+		wget --progress=dot:giga -O - http://download1.graphhopper.com/public/photon-db-latest.tar.bz2 | pbzip2 -cd | tar x
 	fi
-
-	# Check if the download and extraction were successful
-	if [ ! -d "/photon/photon_data/elasticsearch" ]; then
-		handle_error "Search index directory does not exist after extraction"
-	fi
-else
-	echo "Search index directory already exists"
 fi
 
 # Start photon if elastic index exists
 if [ -d "/photon/photon_data/elasticsearch" ]; then
-	echo "Starting Photon"
-	java -jar photon.jar "$@" || handle_error "Failed to start Photon"
+	echo "Start photon"
+	java -jar photon.jar $@
 else
-	handle_error "Could not start Photon, the search index could not be found"
+	echo "Could not start photon, the search index could not be found"
 fi
