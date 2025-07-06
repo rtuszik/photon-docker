10ded8d311ea534b23fa53fc49970820f1fd855a
Robin Tuszik <mail@robin.gg>
2025-07-06 16:49:54 +0200

    refactor(downloader): improve import paths and add MD5 check skip option

--- src/downloader.py
+++ src/downloader.py
@@ -1,11 +1,13 @@
-import config
-import sys
-from logger import get_logger
-from process import stop_photon
-from filesystem import extract_index, move_index, verify_checksum
 import os
+import sys
+
 import requests
 import tqdm
+from process import stop_photon
+
+from filesystem import extract_index, move_index, verify_checksum
+from utils import config
+from utils.logger import get_logger
 
 logging = get_logger()
 
@@ -20,20 +22,21 @@ def parallel_update():
                 logging.debug(f"Successfully removed directory: {config.TEMP_DIR}")
             except Exception as e:
                 logging.error(f"Failed to remove existing TEMP_DIR: {e}")
-                raise # Re-raising the exception after logging it
+                raise 
 
         logging.debug(f"Creating temporary directory: {config.TEMP_DIR}")
         os.makedir(config.TEMP_DIR, exist_ok=True)
 
-        logging.info("Downloading index and MD5 checksum...")
+        logging.info("Downloading index")
         download_index()
-        download_md5()
 
-        logging.info("Verifying checksum...")
+        if not config.SKIP_MD5_CHECK:
+            download_md5()
 
-        verify_checksum()
+            logging.info("Verifying checksum...")
+            verify_checksum()
 
-        logging.debug("Checksum verification successful.")
+            logging.debug("Checksum verification successful.")
 
         logging.info("Stopping Photon")
         stop_photon()
@@ -66,7 +69,7 @@ def sequential_download():
                 logging.debug(f"Successfully removed directory: {config.TEMP_DIR}")
             except Exception as e:
                 logging.error(f"Failed to remove existing TEMP_DIR: {e}")
-                raise # Re-raising the exception is good practice
+                raise 
 
         logging.debug(f"Creating temporary directory: {config.TEMP_DIR}")
         os.makedir(config.TEMP_DIR, exist_ok=True)
@@ -74,11 +77,14 @@ def sequential_download():
         logging.info("Downloading new index and MD5 checksum...")
         download_index()
         extract_index()
-        download_md5()
 
-        logging.info("Verifying checksum...")
-        verify_checksum()
-        logging.debug("Checksum verification successful.")
+        if not config.SKIP_MD5_CHECK:
+            download_md5()
+
+            logging.info("Verifying checksum...")
+            verify_checksum()
+
+            logging.debug("Checksum verification successful.")
 
         logging.info("Moving new index into place...")
         move_index()
@@ -88,15 +94,15 @@ def sequential_download():
     except Exception as e:
         logging.critical(f"FATAL: Update process failed with an error: {e}")
         logging.critical("Aborting script.")
-        sys.exit(1) # Exit the script with an error code
+        sys.exit(1) 
 
 def download_index():
     
     if config.COUNTRY_CODE:
-        index_file = "extracts/by-country-code/" + config.COUNTRYCODE + "/photon-db-" + config.COUNTRYCODE + "-latest.tar.bz2"
+        index_file = "/extracts/by-country-code/" + config.COUNTRYCODE + "/photon-db-" + config.COUNTRYCODE + "-latest.tar.bz2"
 
     else:
-        index_file = "photon-db-latest.tar.bz2" 
+        index_file = "/photon-db-latest.tar.bz2" 
 
     download_url = config.BASE_URL + index_file
     
@@ -107,9 +113,9 @@ def download_index():
 def download_md5():
 
     if config.COUNTRY_CODE:
-        md5_file = "extracts/by-country-code/" + config.COUNTRYCODE + "/photon-db-" + config.COUNTRYCODE + "-latest.tar.bz2.md5"
+        md5_file = "/extracts/by-country-code/" + config.COUNTRYCODE + "/photon-db-" + config.COUNTRYCODE + "-latest.tar.bz2.md5"
     else: 
-        md5_file = "photon-db-latest.tar.bz2.md5"
+        md5_file = "/photon-db-latest.tar.bz2.md5"
 
     download_url = config.BASE_URL + md5_file
 
