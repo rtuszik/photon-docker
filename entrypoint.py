e7695e0417a40244eec26920cdf45131d8b3e1a9
Robin Tuszik <mail@robin.gg>
2025-07-06 14:37:55 +0200

    refactor(entrypoint): remove unused function `check_for_index` refactor(entrypoint): remove comments that are no longer relevant feat(entrypoint): add configuration validation at startup to ensure correct settings feat(entrypoint): add support for parallel updates based on `UPDATESTRATEGY` fix(entrypoint): ensure cron job is set up before starting photon

--- entrypoint.py
+++ entrypoint.py
@@ -1,33 +1,34 @@
-# contains the container startup logic. 
-# If possible, create cronjob
-# Check for existing index
-# Check for Force update env var
-# Download index if not exists
 
-from src import config
-from src.logger import get_logger
-from src.downloader import sequential_update
+import sys
+from src.utils import config
+from src.utils.validate_config import validate_config
+from src.utils.logger import get_logger
+from src.downloader import sequential_update, parallel_update
 from src.process import start_photon
+from src.cron_setup import setup_cronjob
 import os
 
 logging = get_logger()
 
 
 def main():
+    try:
+        validate_config()
+    except ValueError as e:
+        logging.error(f"Stopping due to invalid configuration.\n{e}")
+        sys.exit(1) 
+
     if not config.FORCE_UPDATE and os.path.isdir(config.OS_NODE_DIR):
         logging.info("Existing index found")
-        start_cronjob()
+        setup_cronjob()
         start_photon()
-    else: 
+
+    elif config.UPDATESTRATEGY == "SEQUENTIAL":
+        parallel_update()
+        setup_cronjob()
+        start_photon()
+    
+    else:
+        setup_cronjob()
+        start_photon()
         sequential_update()
-        start_photon()
-
-
-
-def check_for_index():
-    if os.path.isdir(config.OS_NODE_DIR):
-        existing 
-    #verifies structure of the final extracted index
-
-
-
