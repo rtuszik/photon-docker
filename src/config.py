4f98c7ba0d90a7565349ba2fead95fdd9307e6de
Robin Tuszik <mail@robin.gg>
2025-07-06 11:42:48 +0200

    draft of config module

--- src/config.py
+++ src/config.py
@@ -7,13 +7,11 @@ FILE_URL=os.getenv("FILE_URL")
 PHOTON_PARAMS=os.getenv("PHOTON_PARAMS")
 JAVA_PARAMS=os.getenv("JAVA_PARAMS")
 LOG_LEVEL=os.getenv("LOG_LEVEL", "INFO")
-COUNTRY_CODE=os.getenv("COUNTRY_CODE")
+COUNTRY_CODE=os.getenv("COUNTRY_CODE").lower()
 
 # APP CONFIG
-PHOTON_DIR =  "/photon"
-PHOTON_DATA_DIR = PHOTON_DIR + "/photon_data"
-TEMP_DIR = PHOTON_DIR + "/temp"
-
-OS_NODE_DIR = PHOTON_DATA_DIR + "/node_1"
-
-PID_FILE= PHOTON_DIR + "/photon.pid"
+PHOTON_DIR = "/photon"
+PHOTON_DATA_DIR = os.path.join(PHOTON_DIR, "photon_data")
+TEMP_DIR = os.path.join(PHOTON_DIR, "temp")
+OS_NODE_DIR = os.path.join(PHOTON_DATA_DIR, "node_1")
+PID_FILE = os.path.join(PHOTON_DIR, "photon.pid")
