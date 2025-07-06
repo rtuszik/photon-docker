d32b6605eec9ed79a0db49977c73c6b7766400ef
Robin Tuszik <mail@robin.gg>
2025-07-06 16:49:04 +0200

    refactor: remove unused process.py and dead code from updater.py

--- updater.py
+++ updater.py
@@ -1,7 +1,3 @@
-# this is the entrypoint for the update script
-# it should by called on the specified schedule with cronjob
-# it will make use of the python-pidfile library to prevent multiple instances at the same time
-
 from src.utils.logger import get_logger
 from src import config
 from src.downloader import parallel_update, sequential_update
@@ -25,11 +21,4 @@ def main():
     else:
         sequential_update()
         start_photon()
-
-
-# check for existance of pidfile and run if no other instance running
-# check if remote file newer than local file
-# create update based on local function
-
-
 
