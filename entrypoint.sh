3a940da8e65d917c4147611151fd56afbdd004bf
Robin Tuszik <mail@robin.gg>
2025-08-05 17:45:59 +0200

    refactor(entrypoint): migrate node_1 data to photon_data directory and remove old index

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 
-chown -R photon:photon /photon
+if [ -d "/photon/data/photon_data/node_1" ]; then
+    if [ -d "/photon/data/node_1" ]; then
+        echo "Removing old index..."
+        rm -rf /photon/data/node_1
+        echo "Cleanup complete: removed /photon/data/node_1"
+    fi
+elif [ -d "/photon/data/node_1" ]; then
+    echo "Migrating data structure..."
+    mkdir -p /photon/data/photon_data
+    mv /photon/data/node_1 /photon/data/photon_data/
+    echo "Migration complete: moved node_1 to /photon/data/photon_data/"
+fi
 
+chown -R photon:photon /photon
 exec gosu photon "$@"
